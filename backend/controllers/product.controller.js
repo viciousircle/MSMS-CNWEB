7f12408d54989b65095681a6dd5619bebde51450
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2025-03-18 19:50:03 +0700

    Test config DB

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -15,26 +15,46 @@ const getProducts = asyncHandler(async (req, res) => {
 // @route: POST /api/products
 // @access: Private
 const setProduct = asyncHandler(async (req, res) => {
-    if (!req.body.text) {
-        return res.status(400).json({ error: "Please provide a text" });
+    if (!req.body.name) {
+        return res.status(400).json({ error: "Please provide a name" });
     }
 
-    res.status(200).json({ message: "Set data successfully" });
+    const product = await Product.create({ name: req.body.name });
+
+    res.status(200).json(product);
 });
 
 // @desc: Update a product
 // @route: PUT /api/products/:id
 // @access: Private
 const updateProduct = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: `Updated data with id ${req.params.id}`,
-    });
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({ error: "Product not found" });
+    }
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+
+    res.status(200).json(updatedProduct);
 });
 
 // @desc: Delete a product
 // @route: DELETE /api/products/:id
 // @access: Private
 const deleteProducts = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({ error: "Product not found" });
+    }
+
+    await Product.findByIdAndDelete(req.params.id);
+
     res.status(200).json({
         message: `Deleted data with id ${req.params.id}`,
     });
