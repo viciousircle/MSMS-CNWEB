47d8ac08e57d5fd54b909a73e218a0e523490ff4
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2025-03-19 08:29:21 +0700

    Add some template components

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Example from "../components/Example";
+import Banner from "../components/Banner";
+import FlyoutMenus from "../components/FlyoutMenus";
 
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div>
+            Home
+            <Banner />
+            <FlyoutMenus />
+            <Example />
+        </div>
+    );
 };
 
 export default Home;
