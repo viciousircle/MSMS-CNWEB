58b7625878aff1097064a9a23fe563caddfd9058
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2025-03-20 21:06:37 +0700

    Cart next

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import img2 from "../assets/img2.jpeg";
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
+import {
+    ChevronDownIcon,
+    PhoneIcon,
+    PlayCircleIcon,
+} from "@heroicons/react/20/solid";
+import { ShoppingBagIcon, ShoppingCartIcon } from "@heroicons/react/24/outline";
 
 const Cart = () => {
     return (
@@ -12,20 +19,21 @@ const Cart = () => {
                 {/* Title Section */}
                 <div className="relative">
                     <hr className="border-gray-950/5 absolute left-[-100%] right-[-100%] top-0" />
-                    <h1 className="text-5xl text-pretty gap-x-2 inline-flex max-w-none px-8 py-8 tracking-tight whitespace-nowrap">
-                        Cart
+                    <h1 className="text-5xl text-pretty gap-x-2 flex max-w-none  tracking-widest whitespace-nowrap items-center">
+                        <div className="border-r border-gray-950/5 flex items-center">
+                            <ShoppingCartIcon className="size-20 py-1 px-2" />
+                        </div>
+                        <div className="px-2 font-serif">Cart</div>
                     </h1>
                     <hr className="border-gray-950/5 absolute bottom-0 left-[-100%] right-[-100%]" />
                 </div>
 
                 <div className="relative">
                     <hr className="border-gray-950/5 absolute left-[-100%] right-[-100%] top-0" />
-                    <div className="flex gap-4 px-4">
-                        <div className="text-pretty text-sm gap-x-2 inline-flex max-w-none tracking-tight whitespace-nowrap">
-                            <div className="flex border-gray-950/5 border-x gap-2 hover:bg-gray-950/2.5 items-center px-2 py-2">
-                                <div className="border p-2"></div>
-                                <div className="">Products</div>
-                            </div>
+                    <div className="text-gray-700 text-pretty font-medium font-mono px-4 tracking-widest uppercase flex gap-4">
+                        <div className="flex border-gray-950/5 border-x gap-2 hover:bg-gray-950/2.5 items-center px-4 py-2">
+                            <div className="border p-2"></div>
+                            <div className="">Products</div>
                         </div>
                     </div>
 
@@ -36,9 +44,15 @@ const Cart = () => {
                 <div className="flex flex-col gap-4">
                     <div className="relative">
                         <hr className="border-gray-950/5 absolute left-[-100%] right-[-100%] top-0" />
-                        <div className="text-gray-700 text-pretty font-medium font-mono px-6 py-1 tracking-widest uppercase">
-                            12/12/2025
+                        <div className="text-gray-700 text-pretty font-medium font-mono px-4 tracking-widest uppercase flex gap-4">
+                            <div className="flex border-gray-950/5 border-x gap-2 bg-gray-950/2.5 items-center px-4 py-2 w-fit">
+                                <div className="">12/12/2025</div>
+                            </div>
+                            <div className="flex border-gray-950/5 border-x gap-2 bg-gray-950/2.5 items-center px-4 py-2">
+                                <div className="">4 ITEMS</div>
+                            </div>
                         </div>
+
                         <hr className="border-gray-950/5 absolute bottom-0 left-[-100%] right-[-100%]" />
                     </div>
 
@@ -118,6 +132,18 @@ const Cart = () => {
 
             {/* Right Decoration */}
             <div className="bg-[image:repeating-linear-gradient(315deg,currentColor_0,currentColor_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed border-x text-gray-950/5 px-6"></div>
+
+            {/* Fixed Bill Section */}
+            <div className=" bg-white border-gray-950/5 border-t w-full bottom-0 fixed  items-center left-0  bg-[image:repeating-linear-gradient(45deg,currentColor_0,currentColor_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed border-x text-gray-950/5 flex justify-center">
+                <div className="flex  justify-center bg-white w-fit border-x">
+                    <div className=" font-medium text-black py-4 px-8 bg-gray-950/5 ">
+                        Total: 312.000.000 VND
+                    </div>
+                    <button className="bg-white  text-black border-l border-gray-950/5 cursor-pointer duration-300 hover:bg-black hover:outline hover:text-gray-200  transition  px-16 uppercase tracking-widest font-medium ">
+                        Check out
+                    </button>
+                </div>
+            </div>
         </main>
     );
 };
