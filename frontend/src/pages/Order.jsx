8aaeaa1dfe670aa47e4f9ce8d73eda79ae922501
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2025-03-21 09:52:27 +0700

    Update Order.jsx

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -1,7 +1,298 @@
 import React from "react";
+import img2 from "../assets/img2.jpeg";
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
+import {} from "@heroicons/react/20/solid";
+import {
+    ArchiveBoxIcon,
+    ArrowLongRightIcon,
+    BanknotesIcon,
+    CheckBadgeIcon,
+    ChevronDoubleDownIcon,
+    ChevronUpDownIcon,
+    InboxArrowDownIcon,
+    PencilIcon,
+    ShieldCheckIcon,
+    ShoppingBagIcon,
+    ShoppingCartIcon,
+    SparklesIcon,
+    StarIcon,
+    TruckIcon,
+} from "@heroicons/react/24/outline";
 
 const Order = () => {
-    return <div className="pt-16">Order</div>;
+    return (
+        <main className="flex justify-between w-full overflow-hidden relative ">
+            <div className="bg-[image:repeating-linear-gradient(315deg,currentColor_0,currentColor_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed border-x text-gray-950/5 px-6"></div>
+
+            <div className="flex flex-col w-full gap-8 pt-8 mt-16">
+                <div className="relative">
+                    <hr className="border-gray-950/5 absolute left-[-100%] right-[-100%] top-0" />
+                    <h1 className="text-5xl text-pretty gap-x-2 flex max-w-none  tracking-widest whitespace-nowrap items-center">
+                        <div className="border-r border-gray-950/5 flex items-center">
+                            <InboxArrowDownIcon className="size-20 py-1 px-2" />
+                        </div>
+                        <div className="px-2 font-serif">Order</div>
+                    </h1>
+                    <hr className="border-gray-950/5 absolute bottom-0 left-[-100%] right-[-100%]" />
+                </div>
+
+                <div className="relative">
+                    <hr className="border-gray-950/5 absolute left-[-100%] right-[-100%] top-0" />
+                    <div className="text-gray-700 text-pretty font-medium font-mono px-4 tracking-widest uppercase flex gap-4">
+                        <div className="flex border-gray-950/5 border-x  bg-gray-950/2.5 items-center px-4 py-2 justify-between w-full">
+                            <div className="">Order Status </div>
+                        </div>
+                    </div>
+                    <hr className="border-gray-950/5 absolute bottom-0 left-[-100%] right-[-100%]" />
+                </div>
+
+                <div className="relative">
+                    <hr className="border-gray-950/5 absolute left-[-100%] right-[-100%] top-0" />
+
+                    <h1 className="text-5xl text-pretty flex max-w-none  tracking-widest whitespace-nowrap items-center justify-center gap-10">
+                        <div className="border-x border-gray-950/5 flex items-center  flex-col">
+                            <div className="text-lg font-mono bg-gray-950/2.5 border-b w-full text-center border-gray-950/5 ">
+                                <div>0</div>
+                            </div>
+                            <div className=" hover:bg-gray-950/2.5">
+                                <ShieldCheckIcon className="size-20 py-1 px-2" />
+                            </div>
+                        </div>
+                        <div className="border-x border-gray-950/5 flex items-center flex-col">
+                            <div className="text-lg font-mono bg-gray-950/2.5 border-b w-full text-center border-gray-950/5 ">
+                                <div>0</div>
+                            </div>
+                            <div className="hover:bg-gray-950/2.5 ">
+                                <ArchiveBoxIcon className="size-20 py-1 px-2" />
+                            </div>
+                        </div>
+                        <div className="border-x border-gray-950/5 flex items-center flex-col">
+                            <div className="text-lg bg-gray-950/2.5 border-b w-full text-center border-gray-950/5 font-mono">
+                                <div>90</div>
+                            </div>
+                            <div className="hover:bg-gray-950/2.5">
+                                <TruckIcon className="size-20 py-1 px-2" />
+                            </div>
+                        </div>
+                        <div className="border-x border-gray-950/5 flex items-center  flex-col">
+                            <div className="text-lg font-mono bg-gray-950/2.5 border-b w-full text-center border-gray-950/5 ">
+                                <div>0</div>
+                            </div>
+                            <div className="hover:bg-gray-950/2.5">
+                                <SparklesIcon className="size-20 py-1 px-2" />
+                            </div>
+                        </div>
+                    </h1>
+                    <hr className="border-gray-950/5 absolute bottom-0 left-[-100%] right-[-100%]" />
+                </div>
+
+                <div className="flex flex-col gap-0">
+                    <div className="relative">
+                        <hr className="border-gray-950/5 absolute left-[-100%] right-[-100%] top-0" />
+                        <div className="text-gray-700 text-pretty font-medium font-mono px-4 tracking-widest uppercase flex gap-0">
+                            <div className="flex border-gray-950/5 border-x  bg-gray-950/2.5 items-center px-4 py-2 w-1/3 justify-between">
+                                <div className="">Order history </div>
+                            </div>
+                            <div className="flex border-gray-950/5 border-r  hover:bg-gray-950/2.5 items-center px-4 py-2  justify-center w-full">
+                                <div className="">All</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="relative">
+                        <hr className="border-gray-950/5 absolute left-[-100%] right-[-100%] top-0" />
+                        <div className="text-gray-700 text-pretty font-medium font-mono px-4 tracking-widest uppercase flex gap-4">
+                            <div className="flex border-gray-950/5 border-x  hover:bg-gray-950/2.5 items-center px-4 py-2  w-full justify-center">
+                                <div className="">To confirm </div>
+                            </div>
+                            <div className="flex border-gray-950/5 border-x  hover:bg-gray-950/2.5 items-center px-4 py-2  w-full justify-center">
+                                <div className="">To prepare </div>
+                            </div>
+                            <div className="flex border-gray-950/5 border-x  hover:bg-gray-950/2.5 items-center px-4 py-2  w-full justify-center">
+                                <div className="">To delivery </div>
+                            </div>
+                            <div className="flex border-gray-950/5 border-x  hover:bg-gray-950/2.5 items-center px-4 py-2  w-full justify-center">
+                                <div className="">To rate</div>
+                            </div>
+                            <div className="flex border-gray-950/5 border-x  hover:bg-gray-950/2.5 items-center px-4 py-2  w-full justify-center">
+                                <div className="">Cancelled </div>
+                            </div>
+                            <div className="flex border-gray-950/5 border-x  hover:bg-gray-950/2.5 items-center px-4 py-2  w-full justify-center">
+                                <div className="">Return refund </div>
+                            </div>
+                        </div>
+                        <hr className="border-gray-950/5 absolute bottom-0 left-[-100%] right-[-100%]" />
+                    </div>
+                </div>
+
+                {/* MacBook Section */}
+                <div className="flex flex-col gap-4">
+                    <div className="relative">
+                        <hr className="border-gray-950/5 absolute left-[-100%] right-[-100%] top-0" />
+
+                        <div className="text-gray-700 text-pretty font-medium font-mono px-4 tracking-widest uppercase flex gap-4">
+                            <div className="flex border-gray-950/5 border-x gap-2 items-center px-4 py-2 bg-emerald-50">
+                                <div className="text-emerald-600">Rated</div>
+                            </div>
+                            <div className="flex border-gray-950/5 border-x gap-2 bg-gray-950/2.5 items-center px-4 py-2 w-fit">
+                                <div className="">12/12/2025</div>
+                            </div>
+                            <div className="flex border-gray-950/5 border-x gap-2 bg-gray-950/2.5 items-center px-4 py-2">
+                                <div className="">
+                                    <ArrowLongRightIcon className="size-6" />
+                                </div>
+                            </div>
+                            <div className="flex border-gray-950/5 border-x gap-2 bg-gray-950/2.5 items-center px-4 py-2">
+                                <div className=""> 23/03/2025</div>
+                            </div>
+                            <div className="flex border-gray-950/5 border-x gap-2 bg-gray-950/2.5 items-center px-4 py-2">
+                                <div className="">4 ITEMS</div>
+                            </div>
+                            <div className="flex border-gray-950/5 border-x gap-2 bg-gray-950/2.5 items-center px-4 py-2">
+                                <div className="">34.000.000 VND</div>
+                            </div>
+
+                            <div className="flex border-gray-950/5 border-x gap-2 items-center px-4 py-2 bg-gray-950/2.5">
+                                <div className="text-gray-300 flex gap-2">
+                                    <SparklesIcon className="size-6 text-yellow-600" />
+                                    <SparklesIcon className="size-6 text-yellow-600" />
+                                    <SparklesIcon className="size-6 text-yellow-600" />
+                                    <SparklesIcon className="size-6" />
+                                    <SparklesIcon className="size-6" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <hr className="border-gray-950/5 absolute bottom-0 left-[-100%] right-[-100%]" />
+                    </div>
+
+                    {/* Product Grid */}
+                    <div className="text-center relative">
+                        {/* Top Decorative Line */}
+                        <hr className="border-gray-950/5 absolute left-[-100%] right-[-100%] top-0" />
+
+                        {/* Cards Grid */}
+                        <div className="grid grid-cols-1 w-full lg:grid-cols-4">
+                            {[...Array(6)].map((_, index) => (
+                                <div
+                                    key={index}
+                                    className="flex flex-col w-full gap-0"
+                                >
+                                    <Horizon />
+                                    <div className="flex w-full gap-0">
+                                        <Vertical />
+                                        <div className="flex border border-gray-950/5 p-2 w-full gap-4 hover:bg-gray-950/2.5 min-w-max">
+                                            <div className="flex bg-white h-56  justify-center rounded-lg w-full items-center outline outline-gray-950/5">
+                                                <img
+                                                    src={img2}
+                                                    alt="MacBook"
+                                                    className="w-64 object-contain"
+                                                />
+                                            </div>
+                                        </div>
+                                        <Vertical />
+                                    </div>
+                                    <Horizon />
+                                </div>
+                            ))}
+                        </div>
+
+                        {/* Bottom Decorative Line */}
+                        <hr className="border-gray-950/5 absolute bottom-0 left-[-100%] right-[-100%]" />
+                    </div>
+                </div>
+                {/* MacBook Section */}
+                <div className="flex flex-col gap-4">
+                    <div className="relative">
+                        <hr className="border-gray-950/5 absolute left-[-100%] right-[-100%] top-0" />
+
+                        <div className="text-gray-700 text-pretty font-medium font-mono px-4 tracking-widest uppercase flex gap-4">
+                            <div className="flex border-gray-950/5 border-x gap-2 items-center px-4 py-2 bg-red-50">
+                                <div className="text-red-600">Rated</div>
+                            </div>
+                            <div className="flex border-gray-950/5 border-x gap-2 bg-gray-950/2.5 items-center px-4 py-2 w-fit">
+                                <div className="">12/12/2025</div>
+                            </div>
+                            <div className="flex border-gray-950/5 border-x gap-2 bg-gray-950/2.5 items-center px-4 py-2">
+                                <div className="">
+                                    <ArrowLongRightIcon className="size-6" />
+                                </div>
+                            </div>
+                            <div className="flex border-gray-950/5 border-x gap-2 bg-gray-950/2.5 items-center px-4 py-2">
+                                <div className=""> 23/03/2025</div>
+                            </div>
+                            <div className="flex border-gray-950/5 border-x gap-2 bg-gray-950/2.5 items-center px-4 py-2">
+                                <div className="">4 ITEMS</div>
+                            </div>
+                            <div className="flex border-gray-950/5 border-x gap-2 bg-gray-950/2.5 items-center px-4 py-2">
+                                <div className="">34.000.000 VND</div>
+                            </div>
+                        </div>
+
+                        <hr className="border-gray-950/5 absolute bottom-0 left-[-100%] right-[-100%]" />
+                    </div>
+
+                    {/* Product Grid */}
+                    <div className="text-center relative">
+                        {/* Top Decorative Line */}
+                        <hr className="border-gray-950/5 absolute left-[-100%] right-[-100%] top-0" />
+
+                        {/* Cards Grid */}
+                        <div className="grid grid-cols-1 w-full lg:grid-cols-4">
+                            {[...Array(6)].map((_, index) => (
+                                <div
+                                    key={index}
+                                    className="flex flex-col w-full gap-0"
+                                >
+                                    <Horizon />
+                                    <div className="flex w-full gap-0">
+                                        <Vertical />
+                                        <div className="flex border border-gray-950/5 p-2 w-full gap-4 hover:bg-gray-950/2.5 min-w-max">
+                                            <div className="flex bg-white h-56  justify-center rounded-lg w-full items-center outline outline-gray-950/5">
+                                                <img
+                                                    src={img2}
+                                                    alt="MacBook"
+                                                    className="w-64 object-contain"
+                                                />
+                                            </div>
+                                        </div>
+                                        <Vertical />
+                                    </div>
+                                    <Horizon />
+                                </div>
+                            ))}
+                        </div>
+
+                        {/* Bottom Decorative Line */}
+                        <hr className="border-gray-950/5 absolute bottom-0 left-[-100%] right-[-100%]" />
+                    </div>
+                </div>
+
+                {/* Footer Section */}
+                <div className="text-center relative h-dvh">
+                    <hr className="border-gray-950/5 absolute left-[-100%] right-[-100%] top-0" />
+                    <div className="text-lg w-1/2 font-medium py-2">Hi</div>
+                    <hr className="border-gray-950/5 absolute bottom-0 left-[-100%] right-[-100%]" />
+                </div>
+            </div>
+
+            {/* Right Decoration */}
+            <div className="bg-[image:repeating-linear-gradient(315deg,currentColor_0,currentColor_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed border-x text-gray-950/5 px-6"></div>
+        </main>
+    );
 };
 
 export default Order;
+
+function Horizon() {
+    return (
+        <div className="flex w-full gap-0">
+            <div className="p-2"></div>
+            <div className="border-gray-950/5 border-x p-2 w-full"></div>
+            <div className="p-2"></div>
+        </div>
+    );
+}
+
+function Vertical() {
+    return <div className="border-gray-950/5 border-y p-2"></div>;
+}
