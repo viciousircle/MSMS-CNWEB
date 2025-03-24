71b79423a025c217884b89d124273fe47efb3566
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2025-03-24 13:07:44 +0700

    Update Test case product

--- backend/tests/product.test.js
+++ backend/tests/product.test.js
@@ -6,6 +6,7 @@ const request = require("supertest");
 const mongoose = require("mongoose");
 const { app } = require("../server");
 const Product = require("../models/product.model");
+const User = require("../models/user.model");
 const { MongoMemoryServer } = require("mongodb-memory-server");
 
 let mongoServer;
@@ -14,6 +15,7 @@ beforeAll(async () => {
     mongoServer = await MongoMemoryServer.create();
     const mongoUri = mongoServer.getUri();
     await mongoose.connect(mongoUri);
+    await User.deleteMany({});
 });
 
 afterEach(async () => {
@@ -129,27 +131,45 @@ describe("GET /api/products/:id (Get a product by ID)", () => {
 });
 
 describe("PUT /api/products/:id", () => {
-    // it("should update the product successfully", async () => {
-    //     const product = await Product.create({
-    //         _id: new mongoose.Types.ObjectId(),
-    //         name: "Sample",
-    //         image: "https://example.com/sample.jpg",
-    //         price: 100,
-    //         stock: 2000,
-    //         rate: 4.5,
-    //     });
+    it("should update the product successfully", async () => {
+        const bcrypt = require("bcryptjs");
 
-    //     const respone = await request(app)
-    //         .put(`/api/products/${product._id}`)
-    //         .send({
-    //             name: "Updated",
-    //             price: 200,
-    //         })
-    //         .expect(200);
+        const seller = await User.create({
+            name: "Seller",
+            email: "seller@example.com",
+            password: await bcrypt.hash("password", 10),
+            role: "seller",
+        });
 
-    //     expect(respone.body.name).toBe("Updated");
-    //     expect(respone.body.price).toBe(200);
-    // });
+        const loginResponse = await request(app).post("/api/users/login").send({
+            email: "seller@example.com",
+            password: "password",
+        });
+
+        const token = loginResponse.body.token;
+        expect(token).toBeDefined();
+
+        const product = await Product.create({
+            _id: new mongoose.Types.ObjectId(),
+            name: "Sample",
+            image: "https://example.com/sample.jpg",
+            price: 100,
+            stock: 2000,
+            rate: 4.5,
+        });
+
+        const response = await request(app)
+            .put(`/api/products/${product._id}`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                name: "Updated",
+                price: 200,
+            })
+            .expect(200);
+
+        expect(response.body.name).toBe("Updated");
+        expect(response.body.price).toBe(200);
+    });
 
     it("should return 404 if the product not found", async () => {});
 
