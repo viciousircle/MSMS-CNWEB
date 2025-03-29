034424fa41dc13e80e0cbdca4b719dc23df87161
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2025-03-29 22:03:25 +0700

    Add to cart

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,169 @@
+import React from "react";
+import img2 from "../assets/img2.jpeg";
+import { Button } from "@/components/ui/button";
+import { Minus, Plus } from "lucide-react";
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer";
+
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+
+const CardProduct = () => {
+    return (
+        <div className="flex flex-col w-full gap-0">
+            <div className="flex w-full gap-0">
+                <div className="p-2"></div>
+                <div className="border-gray-950/5 border-x p-2 w-full"></div>
+                <div className="p-2"></div>
+            </div>
+            <div className="flex w-full gap-0">
+                <div className="border-gray-950/5 border-y p-2"></div>
+                <div className="flex flex-col border border-gray-950/5 p-2 w-full gap-4 hover:bg-gray-950/2.5 min-w-max">
+                    <div className="flex bg-white h-64 justify-center rounded-lg w-full items-center outline outline-gray-950/5">
+                        <img
+                            src={img2}
+                            alt="MacBook"
+                            className="w-64 object-contain"
+                        />
+                    </div>
+                    <div className=" pb-2 flex justify-between items-center">
+                        <div className="flex flex-col gap-2 items-start">
+                            <div className="font-medium">MacBook Pro</div>
+                            <div className="text-gray-400 text-xs font-mono">
+                                39.000.000
+                            </div>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <DrawerDemo />
+                        </div>
+                    </div>
+                </div>
+                <div className="border-gray-950/5 border-y p-2"></div>
+            </div>
+            <div className="flex w-full gap-0">
+                <div className="p-2"></div>
+                <div className="border-gray-950/5 border-x p-2 w-full"></div>
+                <div className="p-2"></div>
+            </div>
+        </div>
+    );
+};
+
+export { CardProduct };
+
+export function DrawerDemo() {
+    const [goal, setGoal] = React.useState(1);
+
+    function onClick(adjustment) {
+        setGoal((prev) => Math.max(1, Math.min(10, prev + adjustment)));
+    }
+
+    return (
+        <Drawer>
+            <DrawerTrigger asChild>
+                <Button variant="outline">View detail</Button>
+            </DrawerTrigger>
+            <DrawerContent>
+                <div className="mx-auto w-full max-w-sm">
+                    <DrawerHeader>
+                        <DrawerTitle>Product Options</DrawerTitle>
+                    </DrawerHeader>
+                    {/* Color Selection */}
+                    <div className="p-4 flex gap-4 items-center">
+                        <div className="block text-sm font-medium text-gray-700">
+                            Select Color:
+                        </div>
+                        <Select>
+                            <SelectTrigger className="w-[180px]">
+                                <SelectValue placeholder="Colors" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="light">
+                                    <div className="p-2 bg-black rounded-full"></div>
+                                    <div>Black</div>
+                                </SelectItem>
+                                <SelectItem value="light">
+                                    <div className="p-2 bg-rose-600 rounded-full"></div>
+                                    <div>Rose</div>
+                                </SelectItem>
+                                <SelectItem value="light">
+                                    <div className="p-2 bg-amber-500 rounded-full"></div>
+                                    <div>Amber</div>
+                                </SelectItem>
+                            </SelectContent>
+                        </Select>
+                    </div>
+                    <div className="p-4 text-sm text-gray-500">
+                        Available: 10
+                    </div>
+                    {/* Quantity Selection */}
+                    <div className="p-4 pb-2">
+                        <div className="flex items-center justify-center space-x-2">
+                            <Button
+                                variant="outline"
+                                size="icon"
+                                className="h-8 w-8 shrink-0 rounded-full"
+                                onClick={() => onClick(-1)}
+                                disabled={goal <= 1}
+                            >
+                                <Minus />
+                                <span className="sr-only">Decrease</span>
+                            </Button>
+                            <div className="flex-1 text-center">
+                                <div className="text-7xl font-bold tracking-tighter">
+                                    {goal}
+                                </div>
+                                <div className="text-[0.70rem] uppercase text-muted-foreground">
+                                    Quantity
+                                </div>
+                            </div>
+                            <Button
+                                variant="outline"
+                                size="icon"
+                                className="h-8 w-8 shrink-0 rounded-full"
+                                onClick={() => onClick(1)}
+                                disabled={goal >= 10}
+                            >
+                                <Plus />
+                                <span className="sr-only">Increase</span>
+                            </Button>
+                        </div>
+                    </div>
+
+                    {/* Footer */}
+                    <DrawerFooter>
+                        <div className="flex w-full gap-2">
+                            <Button className="flex-1 hover:bg-emerald-600 hover:text-white bg-white border-emerald-600 border text-emerald-600">
+                                Add to Cart
+                            </Button>
+                            <Button className="flex-1 ">Order now</Button>
+                        </div>
+                        <DrawerClose asChild>
+                            <Button
+                                variant="outline"
+                                className={
+                                    "border-gray-600 text-gray-600 hover:bg-gray-600 hover:text-white"
+                                }
+                            >
+                                Cancel
+                            </Button>
+                        </DrawerClose>
+                    </DrawerFooter>
+                </div>
+            </DrawerContent>
+        </Drawer>
+    );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,29 +1,16 @@
 import React from "react";
-import img2 from "../assets/img2.jpeg";
-import Decoration from "../components/Decoration";
-import { HrBot, HrTop } from "../components/HorizontalLine";
-import { Button } from "@/components/ui/button";
-import {
-    Drawer,
-    DrawerClose,
-    DrawerContent,
-    DrawerDescription,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerTitle,
-    DrawerTrigger,
-} from "@/components/ui/drawer";
+import { CardProduct } from "../components/Card";
 
 const Home = () => {
     return (
         <main className="flex justify-between w-full overflow-hidden relative  ">
-            <Decoration />
+            <div className="decoration-col"></div>
             <div className="flex flex-col w-full gap-16 pt-16 px-4 pb-96">
                 <TitleSection />
 
                 <div className="flex flex-col gap-4">
                     <div className="text-center relative">
-                        <HrTop />
+                        <hr className="hr-top" />
                         <div className="grid grid-cols-1 w-full lg:grid-cols-4 sm:grid-cols-2">
                             {[...Array(8)].map((_, index) => (
                                 <div key={index}>
@@ -31,11 +18,11 @@ const Home = () => {
                                 </div>
                             ))}
                         </div>
-                        <HrBot />
+                        <hr className="hr-bot" />
                     </div>
                 </div>
             </div>
-            <Decoration />
+            <div className="decoration-col"></div>
         </main>
     );
 };
@@ -45,50 +32,14 @@ export default Home;
 function TitleSection() {
     return (
         <div className="text-center relative">
-            <HrTop />
+            <hr className="hr-top" />
             <h1 className="text-5xl text-pretty gap-x-2 inline-flex max-w-none px-8 py-8 tracking-tight whitespace-nowrap">
                 Vicious Store.
                 <span className="text-gray-500">
                     Cách tốt nhất để mua sản phẩm bạn thích
                 </span>
             </h1>
-            <HrBot />
-        </div>
-    );
-}
-
-function CardProduct() {
-    return (
-        <div className="flex flex-col w-full gap-0">
-            <div className="flex w-full gap-0">
-                <div className="p-2"></div>
-                <div className="border-gray-950/5 border-x p-2 w-full"></div>
-                <div className="p-2"></div>
-            </div>
-            <div className="flex w-full gap-0">
-                <div className="border-gray-950/5 border-y p-2"></div>
-                <div className="flex flex-col border border-gray-950/5 p-2 w-full gap-4 hover:bg-gray-950/5 min-w-max">
-                    <div className="flex bg-white h-64 justify-center rounded-lg w-full items-center outline outline-gray-950/5">
-                        <img
-                            src={img2}
-                            alt="MacBook"
-                            className="w-64 object-contain"
-                        />
-                    </div>
-                    <div className="flex flex-col gap-2 items-start pb-2">
-                        <div className="font-medium">MacBook Pro</div>
-                        <div className="text-gray-400 text-xs font-mono">
-                            39.000.000
-                        </div>
-                    </div>
-                </div>
-                <div className="border-gray-950/5 border-y p-2"></div>
-            </div>
-            <div className="flex w-full gap-0">
-                <div className="p-2"></div>
-                <div className="border-gray-950/5 border-x p-2 w-full"></div>
-                <div className="p-2"></div>
-            </div>
+            <hr className="hr-bot" />
         </div>
     );
 }
