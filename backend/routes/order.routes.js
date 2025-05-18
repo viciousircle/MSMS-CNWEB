6baa6040b115a15ad2c6f311fe9168d1e3c32d11
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2025-05-18 22:37:47 +0700

    Update order.routes.js

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const {
     getOrdersForCustomer,
     createOrder,
+    cancelOrder,
 } = require('../controllers/order.controller');
 
 const { protect, authorize } = require('../middleware/auth.middleware');
 
-// //* Private Routes (Only Seller)
 router
     .route('/')
     .get(protect, authorize('customer'), getOrdersForCustomer)
     .post(protect, authorize('customer'), createOrder);
+router.route('/:id/cancel').put(protect, authorize('customer'), cancelOrder);
 
 module.exports = router;
