a1a455eb072ddd1ede97c0a74243d5bd559eae7d
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2025-05-22 08:05:48 +0700

    Update Body.jsx

--- frontend/src/components/Structure/Body.jsx
+++ frontend/src/components/Structure/Body.jsx
@@ -5,7 +5,7 @@ const Body = ({ children }) => {
         <>
             <main className="flex justify-between w-full overflow-hidden relative  ">
                 <div className="decoration-col"></div>
-                <div className="flex flex-col w-full gap-8  pb-96 pt-24">
+                <div className="flex flex-col w-full gap-8  py-24">
                     {children}
                 </div>
                 <div className="decoration-col"></div>
