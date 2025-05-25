0fd4a84cd27c669c8071bc7bfb17b04b59f4c06a
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2025-05-25 15:43:02 +0700

    deploy 3

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+        './app/**/*.{js,ts,jsx,tsx}',
+        './node_modules/@shadcn/ui/**/*.js',
+    ],
     theme: {
         extend: {
             colors: {
